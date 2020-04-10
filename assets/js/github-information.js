6361d2d997d809e96934ccc438922aee9e78524d
aidant842 <aidant842@gmail.com>
2020-04-10 12:22:31 +0000

    Fix bugs for the gh-user-data & gh-repo-data elements

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -32,6 +32,8 @@ function repoInformationHMTL(repos){
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
     var username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please Enter a GitHub Username</h2>`);
@@ -56,3 +58,5 @@ function fetchGitHubInformation(event) {
 
     });
 }
+
+$(document).ready(fetchGitHubInformation);
